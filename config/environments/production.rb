5bc4caa21e9694a583744c0d3d582ae69e96197f
reginafcompton <reginafcompton@gmail.com>
2016-01-23 14:10:14 -0600

    Change domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
 
-  config.action_mailer.default_url_options = { :host => 'secret-harbor-8473.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'handelatnewberry.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
@@ -71,7 +71,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              "smtp.gmail.com",
     port: 587,
-    domain:               "secret-harbor-8473.herokuapp.com",
+    domain:               "handelatnewberry.herokuapp.com",
     authentication:       "plain",
     enable_starttls_auto: true,
     user_name:            ENV['EMAIL'],
